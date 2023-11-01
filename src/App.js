d554ff3ba00202bdce76595ca7f31449016361a4
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-11-01 12:18:53 -0300

    Criando Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 export default function App() {
   return (
     <div>
       <Header/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/misaelguedes" target="blank">&reg; Misael Guedes</a>
+        </footer>
+    )
+}
