4571493b9a25435be273c1ce5141a4d13712086e
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-11-01 14:51:24 -0300

    Montando a estrutura da Section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,31 @@ export default function App() {
   return (
     <div>
       <Header/>
+      <section>
+        <div className="data">
+          <marquee>
+            <div className="dia">
+
+            </div>
+          </marquee>
+        </div>
+        <div className="hora">
+
+        </div>
+        <div className="local">
+          <marquee direction="right" scrollamount="4">
+            <div className="localizacao">
+        
+            </div>
+          </marquee>
+        </div>
+        <div className="clima">
+
+        </div>
+        <div className="imagem">
+          <img/>
+        </div>
+      </section>
       <Footer/>
     </div>
   )
