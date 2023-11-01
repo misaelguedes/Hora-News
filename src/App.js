721df402932846903bcb62ce5f79fd30619bf731
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-11-01 12:11:51 -0300

    Criando Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <h1>Hora do Dia</h1>
+    )
+}
