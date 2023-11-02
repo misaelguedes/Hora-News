cc7e6c783ecc0cdc332d3e496599f73c6895ef74
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-11-02 16:35:15 -0300

    Inserindo a data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -13,18 +14,47 @@ export default function App() {
   const minutos = data.getMinutes()
   const segundos = data.getSeconds()
 
+  const diasSemana = ['Domingo', 'Segunda-feita', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+  const diaSemana = diasSemana[diaSem]
+  const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+  const nomeMes = meses[mes]
+
+  const [classHoras, setClassHoras] = useState("")
+
+  const [time, setTime] = useState(new Date());
+
+  const updateTimeClass = () => {
+    if (horas < 6 || horas >= 18) {
+      setClassHoras('horaNoturna');
+    } else {
+      setClassHoras('horaDiurna');
+    }
+  }
+
+  useEffect(() => {
+    updateTimeClass()
+
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div>
       <Header/>
       <section>
         <div className="data">
           <marquee>
-            <div className="dia">
-
+            <div>
+              {diaSemana}, {diaMes} de {nomeMes} de {ano}
             </div>
           </marquee>
         </div>
-        <div className="hora">
+        <div className={`hora ${classHoras}`}>
           {horas < 10 ? `0${horas}` : horas} : {minutos < 10 ? `0${minutos}` : minutos} : {segundos < 10 ? `0${segundos}` : segundos}
         </div>
         <div className="local">
